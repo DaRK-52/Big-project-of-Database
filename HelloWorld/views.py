d90b920240698a67165b059540c3309a4521eeff
DaRK52 <zhuwenjun@sjtu.edu.cn>
2021-02-25 22:02:09 +0800

    nothing

--- HelloWorld/views.py
+++ HelloWorld/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    views_name="Helloqaq"
+    # views_name={"title":"asdasdasd"}
+    views_name=0
     return render(request,'dark52.html',{"name":views_name})
 # "name":views_name html变量名->views变量名
